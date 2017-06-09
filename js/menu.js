ee90863c0417be9933c14f70f483826e358d2c7b
malwarr <malwarr@iu.edu>
2017-06-09 01:31:05 -0400

    modify ul .menu to respond. add js hb function

--- js/menu.js
+++ js/menu.js
@@ -7,18 +7,28 @@ var menu = document.getElementsByClassName('menu')
 
 // show menu when clicking hamburger button
 hb[0].addEventListener( 'click', function showMenu () {
+  if (window.innerWidth < 769) {
     x[0].style.display = 'inline-block';
     hb[0].style.display = 'none';
     menu[0].style.display = 'block';
+  }
 })
 
 // hide menu when click the x button
 x[0].addEventListener( 'click', function hideMenu () {
-  x[0].style.display = 'none';
-  hb[0].style.display = 'inline-block';
-  menu[0].style.display = 'none';
+  if (window.innerWidth < 769) {
+    x[0].style.display = 'none';
+    hb[0].style.display = 'inline-block';
+    menu[0].style.display = 'none';
+  } 
 })
 
 // hide x button and menu on load
 menu[0].style.display = 'none';
 x[0].style.display = 'none';
+
+if (window.innerWidth > 768) {
+    hb[0].style.display = 'none';
+    x[0].style.display = 'none';
+    menu[0].style.display = 'inline-block';
+  } 
