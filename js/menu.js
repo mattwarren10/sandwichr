c2f4c436b93b59c36c7492186f783257fbc09929
malwarr <malwarr@iu.edu>
2017-06-08 22:40:11 -0400

    add navigation w/ js and css. add normalize.

---
+++ js/menu.js
@@ -0,0 +1,24 @@
+// CREATE JS HAMBURGER MENU
+
+var hb = document.getElementsByClassName('hb');
+var x = document.getElementsByClassName('x');
+var menu = document.getElementsByClassName('menu')
+
+
+// show menu when clicking hamburger button
+hb[0].addEventListener( 'click', function showMenu () {
+    x[0].style.display = 'inline-block';
+    hb[0].style.display = 'none';
+    menu[0].style.display = 'block';
+})
+
+// hide menu when click the x button
+x[0].addEventListener( 'click', function hideMenu () {
+  x[0].style.display = 'none';
+  hb[0].style.display = 'inline-block';
+  menu[0].style.display = 'none';
+})
+
+// hide x button and menu on load
+menu[0].style.display = 'none';
+x[0].style.display = 'none';
